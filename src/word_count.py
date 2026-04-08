0ca1aa91b0c479fc5d495c70b3cffd08e331a9ae
Eduardo Takei Yaginuma <eduyaginuma10@gmail.com>
2026-04-08 10:36:02 -0300

    Add error handling for missing body in word_count_handler

--- src/word_count.py
+++ src/word_count.py
@@ -3,7 +3,10 @@ def word_count_handler(event, context):
     A simple function that counts
     the number of words in a string
     """
-    msg = event["body"]
+    try:
+        msg = event["body"]
+    except KeyError:
+        return {"error": "no body"}
 
     n_words = len(msg.split())
 
